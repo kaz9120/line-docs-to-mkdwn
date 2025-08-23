ef20c57660e4e90eb5b9ab995dc10719cabaed5e
Kazumasa Yamamoto <kazumasa.yamamoto@themosh.jp>
2025-08-23 12:16:19 +0900

    biome の導入

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,3 @@
-declare module 'turndown-plugin-gfm' {
-    export const gfm: any;
+declare module "turndown-plugin-gfm" {
+	export const gfm: any;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,26 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        content: resolve(__dirname, 'src/content.ts')
-      },
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
-      }
-    },
-    lib: {
-      entry: resolve(__dirname, 'src/content.ts'),
-      formats: ['iife'],
-      name: 'ContentScript'
-    }
-  },
-  define: {
-    'process.env': {}
-  }
-})
+	build: {
+		outDir: "dist",
+		rollupOptions: {
+			input: {
+				content: resolve(__dirname, "src/content.ts"),
+			},
+			output: {
+				entryFileNames: "[name].js",
+				chunkFileNames: "[name].js",
+				assetFileNames: "[name].[ext]",
+			},
+		},
+		lib: {
+			entry: resolve(__dirname, "src/content.ts"),
+			formats: ["iife"],
+			name: "ContentScript",
+		},
+	},
+	define: {
+		"process.env": {},
+	},
+});
