21f221873eb816801d897b9217fb9896a97d4e80
Kazumasa Yamamoto <kazumasa.yamamoto@themosh.jp>
2025-08-27 00:09:02 +0900

    Fix: biome のルールを修正

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,25 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	build: {
-		outDir: "dist",
-		rollupOptions: {
-			input: {
-				content: resolve(__dirname, "src/content.ts"),
-			},
-			output: {
-				entryFileNames: "[name].js",
-				chunkFileNames: "[name].js",
-				assetFileNames: "[name].[ext]",
-			},
-		},
-		lib: {
-			entry: resolve(__dirname, "src/content.ts"),
-			formats: ["iife"],
-			name: "ContentScript",
-		},
-	},
-	define: {
-		"process.env": {},
-	},
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        content: resolve(__dirname, "src/content.ts"),
+      },
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+    lib: {
+      entry: resolve(__dirname, "src/content.ts"),
+      formats: ["iife"],
+      name: "ContentScript",
+    },
+  },
+  define: {
+    "process.env": {},
+  },
 });
